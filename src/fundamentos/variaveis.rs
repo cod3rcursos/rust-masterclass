05aae727a6719d2b9d0dd6a77ab3cf1748fd7da2
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2024-03-22 02:14:43 -0300

    Variáveis

--- src/fundamentos/variaveis.rs
+++ src/fundamentos/variaveis.rs
@@ -1,3 +1,50 @@
 pub fn executar() {
-    println!("Variáveis em Rust!");
+    variavel_imutavel();
+    variavel_mutavel();
+
+    // Constante
+    const Z: i32 = 20;
+    println!("O valor de Z é: {}", Z);
+
+    // Shadowing
+    let a = 25;
+    println!("O valor de a é: {}", a);
+
+    let a = 30;
+    println!("O valor de a é: {}", a);
+
+    let a = "Texto";
+    println!("O valor de a é: {}", a);
+
+    let a = 'A';
+    println!("O valor de a é: {}", a);
+
+    let a = 3.14;
+    println!("O valor de a é: {}", a);
+
+    let a = true;
+    println!("O valor de a é: {}", a);
+
+    let a = [1, 2, 3, 4, 5];
+    println!("O valor de a é: {:?}", a);
+
+    let a = (1, 2, 3, 4, 5);
+    println!("O valor de a é: {:?}", a);
+
+
+}
+
+fn variavel_imutavel() {
+    let x = 5;
+    // x = 10;
+    println!("x => {}", x);
+}
+
+fn variavel_mutavel() {
+    let mut x = 10;
+    let y = x;
+    println!("x, y => {} {}", x, y);
+    
+    x = 15;
+    println!("x, y => {} {}", x, y);
 }
