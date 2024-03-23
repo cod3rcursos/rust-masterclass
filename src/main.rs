34833a1301e5423fdd1e151fa9a07ed82fbc3846
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2024-03-23 01:01:04 -0300

    projeto em rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 mod fundamentos;
+mod utils;
+
+use std::process::exit;
+use utils::terminal::{esperar_enter, exibir_menu, limpar_tela};
 
 fn main() {
-    fundamentos::executar();
-    println!("Fim do programa!");
+    loop {
+        let itens = vec!["Fundamentos", "Sair"];
+        let opcao = exibir_menu(itens);
+
+        limpar_tela();
+
+        match opcao {
+            1 => fundamentos::executar(),
+            2 => exit(0),
+            _ => println!("Opção inválida!"),
+        }
+
+        esperar_enter();
+    }
 }
---
+++ src/utils/terminal.rs
@@ -0,0 +1,39 @@
+use std::io::Write;
+
+pub fn exibir_menu(itens: Vec<&str>) -> u32 {
+    loop {
+        limpar_tela();
+        exibir_itens(&itens);
+
+        print!("Escolha uma opção: ");
+        std::io::stdout().flush().unwrap();
+
+        let mut opcao = String::new();
+        std::io::stdin().read_line(&mut opcao).unwrap();
+
+        let opcao: u32 = opcao.trim().parse().unwrap();
+
+        if opcao > 0 && opcao <= itens.len() as u32 {
+            return opcao;
+        } else if opcao == itens.len() as u32 + 1 {
+            return 0;
+        } else {
+            println!("Opção inválida!");
+        }
+    }
+}
+
+fn exibir_itens(itens: &Vec<&str>) {
+    for (i, item) in itens.iter().enumerate() {
+        println!("{} - {}", i + 1, item);
+    }
+}
+
+pub fn esperar_enter() {
+    let mut enter = String::new();
+    std::io::stdin().read_line(&mut enter).unwrap();
+}
+
+pub fn limpar_tela() {
+    print!("{esc}c", esc = 27 as char);
+}
