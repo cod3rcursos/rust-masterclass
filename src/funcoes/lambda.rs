b74aa29008b2e0f9fb188dc45020a6ad53105368
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2024-03-23 20:19:06 -0300

    funções lambda

---
+++ src/funcoes/lambda.rs
@@ -0,0 +1,28 @@
+// pub fn exemplo() {}
+
+pub fn exemplo_map() {
+    let base_2: Vec<i32> = (1..=10).map(|x| i32::pow(2, x)).collect();
+    println!("{:?}", base_2);
+}
+
+pub fn exemplo_filter() {
+    let e_primo = |n: i32| -> bool {
+        if n < 2 {
+            return false;
+        }
+
+        for i in 2..n {
+            if n % i == 0 {
+                return false;
+            }
+        }
+
+        true
+    };
+
+    let primos: Vec<i32> = (1..=10000)
+        .filter(|&x| e_primo(x))
+        .filter(|&x| x.to_string().ends_with("7"))
+        .collect();
+    println!("{:?}", primos);
+}
--- src/funcoes/mod.rs
+++ src/funcoes/mod.rs
@@ -1,16 +1,19 @@
-mod funcs;
+mod fns;
+mod lambda;
 
 use crate::utils::terminal::{esperar_enter, exibir_menu, limpar_tela};
 
 pub fn executar() {
     loop {
-        let itens = ["Básicos"];
+        let itens = ["Básicos", "Map", "Filter"];
         let selecionado = exibir_menu("Funções", &itens, false);
 
         limpar_tela();
 
         match selecionado {
-            1 => funcs::exemplo(),
+            1 => fns::exemplo(),
+            2 => lambda::exemplo_map(),
+            3 => lambda::exemplo_filter(),
             _ => break,
         }
 
