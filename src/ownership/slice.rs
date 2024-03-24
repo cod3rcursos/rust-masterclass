b3d6e708033266f356f4f33cabebbb095935bf42
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2024-03-24 14:08:02 -0300

    Ajuste no exemplo de slice

--- src/ownership/slice.rs
+++ src/ownership/slice.rs
@@ -5,15 +5,12 @@
 pub fn exemplo() {
     let texto = String::from("Rust é uma linguagem de programação moderna");
 
-    let palavra = com_slice(&texto, 'l');
-    println!("A palavra é '{}'", palavra);
-
-    let palavra = sem_slice(&texto, 'l');
+    let palavra = primeira_palavra_com('l', &texto);
     println!("A palavra é '{}'", palavra);
 }
 
 // Primeira palavra iniciada pela letra informada
-fn com_slice(slice: &str, letra: char) -> &str {
+fn primeira_palavra_com(letra: char, slice: &str) -> &str {
     let bytes = slice.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == letra as u8 {
@@ -21,27 +18,4 @@ fn com_slice(slice: &str, letra: char) -> &str {
         }
     }
     slice
-}
-
-// Primeira palavra iniciada pela letra informada
-fn sem_slice(texto: &String, letra: char) -> String {
-    let bytes = texto.as_bytes();
-
-    let mut encontrou = false;
-    let mut inicio = 0;
-    let mut fim = 0;
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == letra as u8 {
-            encontrou = true;
-            inicio = i;
-        }
-
-        if encontrou && item == b' ' {
-            fim = i;
-            break;
-        }
-    }
-
-    String::from(if encontrou { &texto[inicio..fim] } else { "" })
 }
